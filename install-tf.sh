8d4ba9075bede9725f33b626cb88b7d450946b32
Jason Poley <jason.poley@gmail.com>
2022-09-17 09:55:58 -0400

    newer tf

--- install-tf.sh
+++ install-tf.sh
@@ -1,12 +1,9 @@
 sudo yum install -y yum-utils
 
-#sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-
-#sudo yum -y install terraform
-
-
-
-wget https://releases.hashicorp.com/terraform/0.14.4/terraform_0.14.4_linux_amd64.zip
-unzip terraform_0.14.4_linux_amd64.zip
+rm ~/bin/terraform
+wget https://releases.hashicorp.com/terraform/1.2.9/terraform_1.2.9_linux_amd64.zip
+unzip terraform_1.2.9_linux_amd64.zip
 mkdir ~/bin
 mv terraform ~/bin
+rm terraform_1.2.9_linux_amd64.zip
+

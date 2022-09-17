85a4cef0e71a78b6be6b21a6b4e25d9087f0590f
Jason Poley <jason.poley@gmail.com>
2022-09-17 10:06:07 -0400

    a fix

--- install-tf.sh
+++ install-tf.sh
@@ -3,7 +3,7 @@ sudo yum install -y yum-utils
 rm ~/bin/terraform
 wget https://releases.hashicorp.com/terraform/1.2.9/terraform_1.2.9_linux_amd64.zip
 unzip terraform_1.2.9_linux_amd64.zip
-mkdir ~/bin
-mv terraform ~/bin
+mkdir -p ~/bin
+mv terraform ~/bin/
 rm terraform_1.2.9_linux_amd64.zip
 
